3d8e6d65959c7828d8d6979f893973150ef20b7d
pblaas <patrick@kite4fun.nl>
2017-10-29 00:48:41 +0200

    update tls script

--- tfgenswarm/tls/gentls.sh
+++ tfgenswarm/tls/gentls.sh
@@ -7,7 +7,7 @@ openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj "/CN
 sed -e s/FLOATING_IP/$1/ openssl.tmpl.cnf > openssl.cnf
 # server cert
 openssl genrsa -out swarmserver-key.pem 2048
-openssl req -new -key swarmserver-key.pem -out swarmserver.csr -subj "/CN=kube-swarmserver" -config openssl.cnf
+openssl req -new -key swarmserver-key.pem -out swarmserver.csr -subj "/CN=swarmserver" -config openssl.cnf
 openssl x509 -req -in swarmserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out swarmserver.pem -days 365 -extensions v3_req -extfile openssl.cnf
 
 #client cert
@@ -15,9 +15,16 @@ openssl genrsa -out client1-key.pem 2048
 openssl req -new -key client1-key.pem -out client1.csr -subj "/CN=client1" -config openssl.cnf
 openssl x509 -req -in client1.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out client1.pem -days 365 -extensions v3_req -extfile openssl.cnf
 
+echo Creating new alias swarm
+alias swarm="docker --tlsverify --tlscacert=ca.pem --tlscert=client1.pem --tlskey=client1-key.pem -H=$1:2376"
 
 #server needs dockerd --tlsverify --tlscacert=ca.pem --tlscert=swarmserver.pem --tlskey=swarmserver-key.pem -H=0.0.0.0:2376
-#docker --tlsverify --tlscacert=ca.pem --tlscert=client1.pem --tlskey=client1.pem -H=$1:2376 version
+#--tlsverify --tlscacert=ca.pem --tlscert=server-cert.pem --tlskey=server-key.pem \
+#  -H=0.0.0.0:2376
+# mkdir -p /etc/systemd/system/docker.service.d
+#
+#docker --tlsverify --tlscacert=ca.pem --tlscert=client1.pem --tlskey=client1-key.pem -H=$1:2376 version
+#
 #
 # mkdir -pv ~/.docker
 # cp -v {ca,cert,key}.pem ~/.docker
