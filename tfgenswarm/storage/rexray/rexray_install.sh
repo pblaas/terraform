89d31c6df9081bdca22d26934d12336395d96659
pblaas <patrick@kite4fun.nl>
2017-10-31 13:09:30 +0100

    update rexray support

---
+++ tfgenswarm/storage/rexray/rexray_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker plugin install rexray/cinder:latest \
+  CINDER_AUTHURL=Default \
+  CINDER_USERNAME=Default \
+  CINDER_PASSWORD=Default \
+  CINDER_TENANTID=Default \
+  CINDER_TENANTNAME="Default" \
+  CINDER_DOMAINNAME=Default \
+  CINDER_REGIONNAME=Default \
+  CINDER_AVAILABILITYZONENAME=AMS-EQ1 \
+  REXRAY_PREEMPT=true \
+  REXRAY_LOGLEVEL=debug
